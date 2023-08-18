e27300f0aa7b541a6ad0ace73d1c0ca30ebfc204
anapa <anapaolamcruz13@gmail.com>
2023-08-17 22:34:27 -0300

    exercício 1 feito

--- app/src/main/java/com/pam/fixacao/exer1.java
+++ app/src/main/java/com/pam/fixacao/exer1.java
@@ -16,6 +16,7 @@ public class exer1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exer1);
 
+        // Nesta parte, está ligado as variáveis com a view desta activity
         editTextA = findViewById(R.id.editTextA);
         editTextB = findViewById(R.id.editTextB);
         textViewResult = findViewById(R.id.textViewResult);
@@ -24,21 +25,30 @@ public class exer1 extends AppCompatActivity {
         btnverificar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // Foram usadas as variável para verificar, todos do tipo "integer"
                 int a = Integer.parseInt(editTextA.getText().toString());
                 int b = Integer.parseInt(editTextB.getText().toString());
+
+                // Métodos de checar/verificar múltiplos:
                 checkMultiples(a, b);
             }
         });
     }
 
     @SuppressLint("SetTextI18n")
+
+    // Criando o método de checar/verificar os multiplos dos valores de a e b
     public void checkMultiples(int a, int b) {
+
+        // Condições/Fórmula utilizada:
         if (a % b == 0 || b % a == 0) {
             textViewResult.setText("São múltiplos");
         } else {
             textViewResult.setText("Não são múltiplos");
         }
     }
+    // Variáveis utilizadas:
     EditText editTextA;
     EditText editTextB;
     TextView textViewResult;
